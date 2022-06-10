6b1364f75e77bcaf62e80c107d587c5b83259add
ELIZA <rysaeva96@mail.ru>
2022-06-11 02:17:11 +0500

    Вывод натуральных чисел от M до N

---
+++ Task_65/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа 
+// в промежутке от M до N.
+
+Console.Write("Задайте число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumber1(int m, int n)
+{
+    if (m > n) return;
+
+    NaturalNumber1(m, n - 1);
+    Console.Write($"{n} ");
+}
+void NaturalNumber2(int n, int m)
+{    if (n > m) return;
+
+    NaturalNumber2(n, m - 1);
+    Console.Write($"{m} ");
+}
+if (m < n) NaturalNumber2(m, n);
+if (n < m) NaturalNumber1(n, m);
