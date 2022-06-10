df8251efc1c480d85a057278d72e454f9dbe5c7a
ELIZA <rysaeva96@mail.ru>
2022-06-11 02:16:14 +0500

    Вывод натуральных чисел до N

---
+++ Task_63/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа 
+// в промежутке от 1 до N.
+
+
+Console.Write("Задайте число: ");
+int number = int.Parse(Console.ReadLine());
+
+// void NaturalNumber(int num)
+// {
+//     if(num == 1) return;
+//     num--;
+//     NaturalNumber(num);
+//     Console.Write($"{num} ");
+
+// }
+// NaturalNumber(number + 1);
+
+void NaturalNumber(int num)
+{
+    if(num == 0) return;
+    NaturalNumber(num - 1);
+    Console.Write($"{num} ");
+
+}
+NaturalNumber(number);
