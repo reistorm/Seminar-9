8bb5fd9a13b1cd5e0d15ca19998bf5260ef3bc54
ELIZA <rysaeva96@mail.ru>
2022-06-11 02:12:58 +0500

    Не до конца

---
+++ Task_65dr/Program.cs
@@ -0,0 +1,28 @@
+﻿//
+
+
+Console.Write("Задайте число M: ");
+int numM = int.Parse(Console.ReadLine());
+Console.Write("Задайте число N: ");
+int numN = int.Parse(Console.ReadLine());
+
+void PrintNumbersUp(int n, int m)
+{
+    if (n == (m - 1)) return;
+    PrintNumbersUp(m, n - 1);
+    Console.Write($"{n} ");
+}
+void PrintNumbersDown(int m, int n)
+{
+    if (m == (n - 1)) return;
+    PrintNumbersUp(m, n + 1);
+    Console.Write($"{n} ");
+}
+if (numN > numM)
+{
+    PrintNumbersUp(numM, numN);
+}
+else
+{
+    PrintNumbersDown(numM, numN);
+}
