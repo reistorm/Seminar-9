2d77c426eaa6ed3cabce59c7dbf75983f759f875
ELIZA <rysaeva96@mail.ru>
2022-06-11 02:32:33 +0500

    Сумма цифр числа

---
+++ Task_67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет принимать 
+// на вход число и возвращать сумму его цифр.
+
+Console.Write("Задайте число: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetNumberSum(int n)
+{
+    int sum = 0;
+    if(n == 0) return 0;
+    sum = n % 10 + GetNumberSum(n/10);
+    return sum;
+}
+
+Console.WriteLine(GetNumberSum(num));
